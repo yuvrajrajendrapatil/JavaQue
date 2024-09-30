c10e236db4558ea48cce1e45a2f21e635373c491
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2024-10-01 00:48:02 +0530

    Solve Prime Number Problem approach 1
    By using Tejus sir approach

---
+++ PrimeNo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class PrimeNo{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any poisitive number");
+        int num = scanner.nextInt();
+        int i;
+
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                break;
+
+            }
+        }
+
+        if (i == (num / 2)) {
+            System.out.println("It is  Prime No");
+
+        } else {
+            System.out.println("It is not Prime No");
+        }
+
+    }
+}
